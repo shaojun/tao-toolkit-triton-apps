90aff08432ef5eda1c16c90d2d8250be9ae642a6
shaojun <157350686@qq.com>
2022-03-28 12:35:54 +0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -83,7 +83,7 @@ wget --content-disposition ${ngc_vehicletypenet} -O ${default_model_download_pat
 # wget --no-check-certificate ${ngc_mcls_classification} -O ${default_model_download_path}/multitask_cls_model/multitask_cls_resnet18.etlt
 
 mkdir -p ${default_model_download_path}/bicycletypenet_model
-cp scripts/bicycletypenet_resnet18_vehicletypenet_pruned.etlt ${default_model_download_path}/bicycletypenet_model/
+cp ${tao_triton_root}/scripts/bicycletypenet_resnet18_vehicletypenet_pruned.etlt ${default_model_download_path}/bicycletypenet_model/
 rm -rf ${default_model_download_path}/*.zip
 
 # Run the server container.
