36ecb0a6a97cfe1498449f3ed59f1482be796e03
shao <157350686@qq.com>
2023-07-08 09:43:00 +0800

    commit server side recent changes.

--- tao_triton/python/device_hub/event_detector.py
+++ tao_triton/python/device_hub/event_detector.py
@@ -363,7 +363,7 @@ class ElectricBicycleEnteringEventDetector(EventDetectorBase):
             temp_image.save(temp_cropped_image_file_full_name)
             t0 = time.time()
             try:
-                raw_infer_results = tao_client.callable_main(['-m', 'elenet_four_classes_230618_tao',
+                raw_infer_results = tao_client.callable_main(['-m', 'elenet_four_classes_230620_tao',
                                                                     '--mode', 'Classification',
                                                                     '-u', '192.168.66.149:8000',
                                                                     '--output_path', './',
